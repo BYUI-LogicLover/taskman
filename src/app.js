ea9bd29aa1794d514e42529cc108ae0902a38823
Travis Nelson <tnelson4@byupathway.edu>
2026-05-24 08:19:24 -0600

    Revert "add root route to redirect to API documentation"
    This reverts commit fcd83f032112d86741e4d323416ae272cb7e3511.

--- src/app.js
+++ src/app.js
@@ -30,8 +30,6 @@ app.use(express.json());
  */
 app.get('/health', (req, res) => res.json({ ok: true }));
 
-app.get('/', (_req, res) => res.redirect('/api-docs'));
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
 app.get('/openapi.json', (_req, res) => res.json(openapiSpec));
 
